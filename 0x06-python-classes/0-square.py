7aa295a9baa9bebc629351a7a18979a2afdb3584
Promgirl65 <olayinkabusolami@gmail.com>
2023-09-26 01:31:47 -0700

    0-square.py

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This is an empty class Square that
+defines a square."""
+
+
+class Square:
+    """This is an empty class Square that
+    defines a square."""
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,3 @@
+Python - Classes and Objects
+Python OOP
+ALX Python Project 0x06
