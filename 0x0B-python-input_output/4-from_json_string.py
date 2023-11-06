17581dcbf266ebe2ffbc55329fd13fbd7b13fb3f
Promgirl65 <107438673+Promgirl65@users.noreply.github.com>
2023-11-06 18:04:27 +0100

    Delete 0x0B-python-input_output directory

--- 0x0B-python-input_output/4-from_json_string.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""define JSON-to-object function."""
-
-import json
-
-def from_json_string(my_str):
-    """returns Python object representation Of a JSON String"""
-    return json.loads(my_str)
