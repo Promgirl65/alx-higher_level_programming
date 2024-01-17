32b20b25fd6599b4675164928b9a3795ad4d7c95
Promgirl65 <olayinkabusolami@gmail.com>
2024-01-17 12:17:22 -0800

    0x0D-SQL_introduction

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- lists all records of table second_table of database hbtn_0c_0 in my MySQL Server
+-- displays score and the name results respectively
+
+SELECT score, name FROM second_table ORDER BY score DESC;
