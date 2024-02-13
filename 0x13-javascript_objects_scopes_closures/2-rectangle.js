ba6bba3598d1425841513ce5aafed8d999d15a37
Promgirl65 <olayinkabusolami@gmail.com>
2024-02-13 13:53:37 -0800

    0x13-javascript_objects_scopes_closures

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/* defines rectangle
+ */
+class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+module.exports = Rectangle;
